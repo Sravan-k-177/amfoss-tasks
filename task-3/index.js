3d388bd3dc97c96532d870929048aafbef67870e
Sravan <kotasravan122706@gmail.com>
2025-01-05 02:54:25 +0530

    adding task-03,  drum website task

---
+++ task-3/index.js
@@ -0,0 +1,34 @@
+function playw() {
+  var audio = document.getElementById("crash");
+  audio.play();
+}
+
+function playa() {
+  var audio = document.getElementById("kick-bass");
+  audio.play();
+}
+
+function plays() {
+  var audio = document.getElementById("snare");
+  audio.play();
+}
+
+function playd() {
+  var audio = document.getElementById("tom-1");
+  audio.play();
+}
+
+function playj() {
+  var audio = document.getElementById("tom-2");
+  audio.play();
+}
+
+function playk() {
+  var audio = document.getElementById("tom-3");
+  audio.play();
+}
+
+function playl() {
+  var audio = document.getElementById("tom-4");
+  audio.play();
+}
