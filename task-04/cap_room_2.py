f36bc0f9730ddb28382c1ca7b1d221525eb64f73
Sravan-k-177 <kotasravan122706@gmail.com>
2025-01-17 23:28:47 +0530

    Update cap_room_2.py

--- task-04/cap_room_2.py
+++ task-04/cap_room_2.py
@@ -7,3 +7,9 @@ for i in set(room_numbers):
     if i > number_of_people//group_size:
         print(i)
         break
+
+
+#assuming a pattern to be always exixtant        
+#since the room number of captain is more than the hightest room number of others
+#this returns thee number which is higher than the room numbers which were alloted to passangers
+#the room numbers list could be sorted and the last element be returned, but the group size is given, so i tried to use it
