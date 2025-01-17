05331844ad5a83b66bab74f40ef33f2b5c4560eb
Sravan-k-177 <kotasravan122706@gmail.com>
2025-01-17 23:30:34 +0530

    Update decoding_digit_frequencies.py

--- task-04/decoding_digit_frequencies.py
+++ task-04/decoding_digit_frequencies.py
@@ -8,3 +8,9 @@ for i in input_string:
         count_numbers[i] += 1
 result = " ".join(map(str,count_numbers.values()))
 print(result)
+
+
+#this creates a dictionary for all the digits from 0 to 9 and then while iterating
+#through the string, it checks for the element being a number and if it is
+#it updates the occurance of the element
+#at last prints the values in the asked format
